d3e85bc4dc75090e36382f33615a32ac17dffe86
cab222 <cabkata@gmail.com>
2012-08-25 15:37:38 -0400

    disjoint sets

---
+++ src/cabkata/sets/DisjointSet.java
@@ -0,0 +1,47 @@
+package cabkata.sets;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class DisjointSet<V> {
+	private Map<V, Set<V>> data = new HashMap<V, Set<V>>();
+	
+	public Set<V> add(V v)
+	{
+		if(!data.containsKey(v))
+		{
+			data.put(v, new HashSet<V>());
+			data.get(v).add(v);
+		}
+		return data.get(v);
+	}
+	
+	public Set<V> union(V item1, V item2)
+	{
+		Set<V> set1 = data.get(item1);
+		Set<V> set2 = data.get(item2);
+		if(set1 == null)
+		{
+			set1 = add(item1);
+		}
+		
+		if(set2 == null)
+		{
+			set2 = add(item2);
+		}
+		
+		set1.addAll(set2);
+		for(V v : set2)
+		{
+			data.put(v, set1);
+		}
+		return set1;
+	}
+	
+	public Set<V> get(V v)
+	{
+		return data.get(v);
+	}
+}
---
+++ testsrc/cabkata/sets/TestDisjointSets.java
@@ -0,0 +1,28 @@
+package cabkata.sets;
+
+import static org.junit.Assert.*;
+
+import java.util.Set;
+
+import org.junit.Test;
+
+public class TestDisjointSets {
+
+	@Test 
+	public void testDisjointSet()
+	{
+		DisjointSet<String> ds = new DisjointSet<String>();
+		String man = "man";
+		String woman = "woman";
+		Set<String> manSet = ds.add(man);
+		assertEquals(manSet.size(), 1);
+		Set<String> womanSet = ds.add(woman);
+		assertEquals(womanSet.size(), 1);
+		Set<String> people = ds.union(man, woman);
+		assertEquals(people.size(), 2);
+		
+		assertEquals(ds.get(man).size(), 2);
+		assertEquals(ds.get(woman).size(), 2);
+		assertEquals(ds.get(woman), ds.get(man));
+	}
+}
